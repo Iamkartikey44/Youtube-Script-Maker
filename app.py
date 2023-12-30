50a56c20dc344470841d991c64f13235775af5b3
Kartikey Tiwari <68707728+Iamkartikey44@users.noreply.github.com>
2023-12-31 00:31:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from utils import generate_script
+
+st.markdown("""
+<style>
+    div.stButton > button:first-child {
+            background-color: #0099ff;
+            color: #ffffff;
+    }
+    div.stButton > button:hover {
+            background-color: #00ff00;
+            color: #FFFFFF;
+    }
+</style>""",unsafe_allow_html=True)
+
+if "API_Key" not in st.session_state:
+    st.session_state["API_Key"] = ''
+
+
+st.title("❤️ YouTube Script Writing Tool")
+
+st.sidebar.title("😎🗝️")
+st.session_state['API_Key'] = st.sidebar.text_input("What's your API Key?",type='password')
+st.sidebar.image('LLM_YT_ScriptMaker\Youtube.jpg',width=300, use_column_width=True)
+
+prompt = st.text_input("Please provide the topic of the video",key="prompt")
+video_length = st.text_input("Expected Video Length 🕒 (in minutes)",key="video_length")
+creativity = st.slider("Words limit ✨ - (0 LOW || 1 HIGH)",0.0,1.0,2.0,step=0.1)
+
+submit = st.button("Generate Script for me")
+
+if submit:
+    
+    if st.session_state['API_Key']:
+        search_result,title,script = generate_script(prompt,video_length,creativity,st.session_state['API_Key'])
+        st.success("Hope you like this script ❤️")
+
+        st.subheader("Title:🔥")
+        st.write(title)
+
+        st.subheader("Your Video Script:📝")
+        st.write(script)
+
+        st.subheader("Check Out - DuckDuckGo Search:🔍")
+        with st.expander('Show me 👀'): 
+            st.info(search_result)
+
+    else:
+        st.error("Ooopssss!!! Please provide API key.....")
+        
+                
